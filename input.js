72ce71784fcb478061df01d4457f622353c207cf
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2020-05-12 06:26:12 -0400

    Third commit

--- input.js
+++ input.js
@@ -1,88 +1,55 @@
-
-function vndToCurrency() {
-    let currencyAmount = moneyInput;
-    return amountConvert = (currencyAmount / exchangeRate).toFixed(2);
-
-}
-function currencyToVnd() {
-    let currencyAmmount = moneyInput;
-    console.log("The VND exchange value is: ", formatCurrency("vi", "VND", ReturnFromFunctionCall));
-    return amountConvert = (currencyAmmount * exchangeRate).toFixed(2);
-
+const USD={
+VND: 23388.85,
+EUR: 0.85,
+KRW: 1223.56,
+USD: 1,
+CNY: 7.10
 }
 function formatCurrency(type1, type2, value) {
     const formatter = new Intl.NumberFormat(type1, {
-        currency: type2,
-        style: "currency"
+      currency: type2,
+      style: "currency"
     });
     return formatter.format(value);
-}
-function convertCurrency() {
-    console.log("1. VND to USD")
-    console.log("2. USD to VND")
-    console.log("3. KRW to VND")
-    console.log("4. VND to KRW")
-    console.log("5. IDR to VND")
-    console.log("6. VND to IDR")
-    console.log("7. EUR to VND")
-    console.log("8. VND to EUR")
+  }
+let result=0;
+const convertBtn=document.getElementById('convertCurrencyBtn');
+
+const amount=document.getElementById('amountInput');
+
+
+
+function convert(){
+    amountInput=parseInt(amount.value);
+    console.log("what is aomount",amount)
+    const fromValue = document.getElementById("currency1").value;
+    const toValue = document.getElementById("currency2").value;
+    const result = ((USD[toValue] / USD[fromValue]) *amountInput).toFixed(2);
+    console.log("How is" + result);
+    const notification = showResult(toValue, result)
     
-
-    
-     var currencyChoose=  console.log("Please choose the option")
-    switch (currencyChoose) {
-        case 1:
-            exchangeRate = 23.208;
-            ReturnFromFunctionCall = vndToCurrency();
-            console.log("The USD exchange value is", formatCurrency("en-US", "USD", ReturnFromFunctionCall));
-            break;
-        case 2:
-            exchangeRate = 23.208;
-            ReturnFromFunctionCall = currencyToVnd();
-            break;
-
-        case 3:
-            exchangeRate = 19.05;
-            ReturnFromFunctionCall = vndToCurrency();
-            console.log("The KRW exchange value is", formatCurrency("ko-KR", "KRW", ReturnFromFunctionCall));
-            break;
-        case 4:
-            exchangeRate = 19.05;
-            ReturnFromFunctionCall = currencyToVnd();
-            break;
-        case 5:
-            exchangeRate = 1.56;
-            ReturnFromFunctionCall = vndToCurrency();
-            console.log("The IDR exchange value is", formatCurrency("id", "IDR", ReturnFromFunctionCall));
-            break;
-        case 6:
-            exchangeRate = 1.56;
-            ReturnFromFunctionCall = currencyToVnd();
-            break;
-        case 7:
-            exchangeRate = 25.234;
-            ReturnFromFunctionCall = vndToCurrency();
-            console.log("The EUR exchange value is", formatCurrency("de-DE", "EUR", ReturnFromFunctionCall));
-            break;
-        case 8:
-            exchangeRate = 25.234;
-            ReturnFromFunctionCall = currencyToVnd();
-            break;
-
-    }
+    document.getElementById("demo").innerHTML = `${parseInt(amountInput)} ${fromValue}=${notification} ${toValue}`;
 }
 
 
 
 
-    function convert() {
-        moneyInput = prompt("Please input the money needs to convert")
-        if (moneyInput = parseInt(moneyInput)) {
-            convertCurrency();
 
-        }
-        else {
-            console.log("Please input the corrected form")
-        }
-    }
-convert();
+  
+
+  const showResult = (toValue, result) => {
+    let convertedValue = "";
+    console.log(toValue);
+    console.log("This is " + result);
+    toValue === "USD" ? convertedValue = formatCurrency('en-US', "USD", result): ""
+    toValue === "VND" ? convertedValue = formatCurrency('vi', "VND", result) : ""
+    toValue === "EUR" ? convertedValue = formatCurrency('de-DE', "EUR", result) : ""
+    
+    toValue === "CNY" ? convertedValue = formatCurrency('zh-CN', "CNY", result) : ""
+
+    console.log("what is convertedValue",convertedValue);
+    return convertedValue;
+  }
+
+
+document.getElementById("convertCurrencyBtn").onclick= function() { convert(); }
